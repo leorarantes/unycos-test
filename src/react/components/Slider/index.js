95f898bcf0f3c89b7cbb5ffc4f5a16d70067f760
Leonardo Arantes <leonardorarantes95@gmail.com>
2023-02-03 11:12:13 -0300

    refactor: refactor slider

--- src/react/components/Slider/index.js
+++ src/react/components/Slider/index.js
@@ -12,53 +12,44 @@ export default function Slider() {
   React.useEffect(() => {
     async function loadComponent() {
       try {
-        //const { data } = await api.getSliderInfo(key);
-        const data = {
-          spotlights: [
-            {
-              _id: 1,
-              title: "Estilos de natación",
-              description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu 
-              lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi 
-              tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque 
-              purus magna, auctor et, sagittis ac.`,
-              image: "https://images.pexels.com/photos/863988/pexels-photo-863988.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            },
-            {
-              _id: 2,
-              title: "Preparación física",
-              description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu 
-              lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi 
-              tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque 
-              purus magna, auctor et, sagittis ac.`,
-              image: "https://images.pexels.com/photos/414029/pexels-photo-414029.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            },
-            {
-              _id: 3,
-              title: "Entrenar para el éxito",
-              description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu 
-              lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi 
-              tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque 
-              purus magna, auctor et, sagittis ac.`,
-              image: "https://images.pexels.com/photos/8688534/pexels-photo-8688534.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            },
-            {
-              _id: 4,
-              title: "Plan de entrenamiento",
-              description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu 
-              lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi 
-              tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque 
-              purus magna, auctor et, sagittis ac.`,
-              image: "https://images.pexels.com/photos/1181615/pexels-photo-1181615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            }
-          ]
-        }
+        const { data } = await api.getSliderInfo(key);
         const spotlights = data.spotlights;
         setInfo([...spotlights]);
         setSelectedSpotlight({...spotlights[0]});
-        console.log(data);
       } catch(error) {
         console.log(error);
+        const spotlights = [
+          {
+            _id: "5e4c9964d4162a18fb16dd5e",
+            title: "Entrena para el éxito",
+            description: "Mireia Belmonte  y su equipo ponen a tu alcance los conocimientos y técnicas detrás del entrenamiento de una campeona olímpica. \nConoce su preparación dentro y fuera del agua de la mano de su entrenador y preparador físico.",
+            order: 1,
+            image: "https://content.unycos.com/courses/natacion/img/spotlights/Mireia-1847.jpg"
+          },
+          {
+            _id: "5e4c997cd4162a18fb16dd60",
+            title: "Estilos de natación",
+            description: "Una perfecta ejecución de estos estilos, unida al control y a la resistencia, te permitirá convertirte en un nadador interdisciplinar. Mireia y su equipo te enseñarán las claves y los secretos para dominarlos todos desde cero.",
+            order: 2,
+            image: "https://content.unycos.com/courses/natacion/img/spotlights/mireia-belmonte-curso-natacion-slide-02.jpg"
+          },
+          {
+            _id: "5e4c9994d4162a18fb16dd65",
+            title: "Preparación física",
+            description: "Estar en forma va mucho más allá de la piscina. Los preparadores físicos de Mireia te proporcionan una rutina de entrenamiento y tablas de ejercicios que mejorarán tu resistencia, fuerza y flexibilidad para rendir al máximo en el agua.",
+            order: 3,
+            image: "https://content.unycos.com/courses/natacion/img/spotlights/mireia-belmonte-curso-natacion-slide-03.jpg"
+          },
+          {
+            _id: "5e4c99a5d4162a18fb16dd69",
+            title: "Virajes y saltos",
+            description: "El manejo de los saltos y virajes mejorará considerablemente tu rendimiento y tu técnica de natación. Los profesionales de fama internacional que asisten a Mireia te enseñan a perfeccionar esta técnica.",
+            order: 4,
+            image: "https://content.unycos.com/courses/natacion/img/spotlights/mireia-belmonte-curso-natacion-slide-04.jpg"
+          }
+        ]
+        setInfo([...spotlights]);
+        setSelectedSpotlight({...spotlights[0]});
       }
     }
     loadComponent();
